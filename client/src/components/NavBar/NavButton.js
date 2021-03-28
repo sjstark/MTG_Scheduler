8536b67264e80899ed1712faba08979354140484
Sam Stark <SamStarkScenic@gmail.com>
2021-03-28 15:17:41 -0400

    cleaned up NavLinks

--- client/src/components/NavBar/NavButton.js
+++ client/src/components/NavBar/NavButton.js
@@ -1,37 +1,14 @@
-import React, { useState, useEffect } from 'react'
-import { useHistory, useLocation } from 'react-router'
+import React from 'react'
 import { NavLink } from 'react-router-dom'
-import { setView } from '../../store/view'
 
 export const NavButton = ({ view, Icon }) => {
-
-  const history = useHistory()
-
-  const location = useLocation()
-
-  const [active, setActive] = useState(location.pathname.split('/')[0] == view.toLowerCase())
-
-
-
-  // useEffect(() => {
-  //   setActive(view == currentView)
-  // }, [view, currentView])
-
-  const navigate = () => {
-    history.push(`/${view.toLowerCase()}`)
-  }
 
   return (
     <NavLink
       className={`navbar__button`}
       to={view.toLowerCase()}
       activeClassName="--active"
-    // onClick={navigate}
     >
-      {active && (
-        <div className="navbar__button-activebar">
-        </div>
-      )}
       <Icon fontSize="large" />
       <span className="navbar__button-text">
         {view}
